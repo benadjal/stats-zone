71791402cd3b3c31efd2f4dda14c9c947084112d
benadjal <adame.benadjal@gmail.com>
2025-07-28 11:05:20 +0200

    ADD : New 'top-players' component displayed on homePage

---
+++ src/app/components/top-players/top-players.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-players',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-players.component.html',
+  styleUrl: './top-players.component.scss'
+})
+export class TopPlayersComponent {
+
+}
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
+import { TopPlayersComponent } from '../../components/top-players/top-players.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [SearchBarComponent],
+  imports: [SearchBarComponent,TopPlayersComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
