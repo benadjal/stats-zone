b8907f7a0eff6f1c757c5a9db4342552f0c9af54
benadjal <adame.benadjal@gmail.com>
2025-07-29 10:55:36 +0200

    UPDATE : NgStyle instead of ngClass for search-bar's border-color

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,19 +1,16 @@
-import { CommonModule, NgClass } from '@angular/common';
+import { NgStyle } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgStyle],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss'
 })
 export class SearchBarComponent {
 
-  @Input() coloredBorder : boolean = false;
-
-  ngOnInit() {
-    console.log(this.coloredBorder)
-  }
+  // @Input() coloredBorder : boolean = false;
+  @Input() color : string = 'grey'
 
 }
