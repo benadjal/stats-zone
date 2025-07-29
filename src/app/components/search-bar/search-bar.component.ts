5ebac8a200e82d1d424c6fd3a6ef397dd374a017
benadjal <adame.benadjal@gmail.com>
2025-07-29 17:54:57 +0200

    UPDATE : search-bar works and filter matching players

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,18 +1,35 @@
-import { NgStyle } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { AsyncPipe, NgStyle } from '@angular/common';
+import { Component, inject, Input } from '@angular/core';
 import { Player } from '../../models/player.model';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, Observable, of, startWith, switchMap, tap } from 'rxjs';
+import { PlayersService } from '../../services/players.service';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [NgStyle],
+  imports: [NgStyle, ReactiveFormsModule, AsyncPipe],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss'
 })
 export class SearchBarComponent {
 
-  @Input() color : string = 'grey';
+  @Input() color: string = 'grey';
 
-  @Input() filtredPlayers : Player[] = [];
+  playerService = inject(PlayersService)
+
+  searchForm: FormGroup = new FormGroup({
+    search: new FormControl('', Validators.required)
+  });
+
+  filtredPlayers$: Observable<Player[]> = this.getPlayerList();
+
+  getPlayerList(): Observable<Player[]> {
+    return this.searchForm.get('search')!.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((search) => this.playerService.getFilteredPlayers(search))
+    )
+  }
 
 }
--- src/app/services/players.service.ts
+++ src/app/services/players.service.ts
@@ -14,4 +14,15 @@ export class PlayersService {
   getTopPlayers(): Observable<Player[]> {
     return of(topPlayersMock)
   }
+
+  getFilteredPlayers(search : string) : Observable<Player[]> {
+    const normalizedSearch = search.toLocaleLowerCase()
+    let matchingPlayers = topPlayersMock.filter((player) => this.matchesNameOrLastName(player,normalizedSearch));
+    return of(matchingPlayers);
+  }
+
+  private matchesNameOrLastName(player : Player, search : string) : boolean {
+    if(!search) return false;
+    return player.name.toLowerCase().includes(search) || player.lastName.toLocaleLowerCase().includes(search)
+  }
 }
