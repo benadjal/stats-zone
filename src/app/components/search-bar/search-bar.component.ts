1b0c1510e1c0907d5d9c0e14f9fcd38e26b2df01
benadjal <adame.benadjal@gmail.com>
2025-07-29 09:55:02 +0200

    UPDATE : New search-bar-component's prop to display border

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss'
 })
 export class SearchBarComponent {
+
+  @Input() coloredBorder : boolean = false;
+
+  ngOnInit() {
+    console.log(this.coloredBorder)
+  }
 
 }
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
 import { TopPlayersComponent } from '../../components/top-players/top-players.component';
 import { FrontPageComponent } from '../../components/front-page/front-page-news.component';
+import { CompareStatsComponent } from '../../components/compare-stats/compare-stats.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [SearchBarComponent,TopPlayersComponent, FrontPageComponent],
+  imports: [SearchBarComponent,TopPlayersComponent, FrontPageComponent,CompareStatsComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
