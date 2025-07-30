d5b7908cc60f6633b981b28e7d55d3fead59adf1
benadjal <adame.benadjal@gmail.com>
2025-07-30 16:18:38 +0200

    UPDATE : method to get player by id

--- src/app/services/players.service.ts
+++ src/app/services/players.service.ts
@@ -15,14 +15,22 @@ export class PlayersService {
     return of(topPlayersMock)
   }
 
-  getFilteredPlayers(search : string) : Observable<Player[]> {
+  getFilteredPlayers(search: string): Observable<Player[]> {
     const normalizedSearch = search.toLocaleLowerCase()
-    let matchingPlayers = topPlayersMock.filter((player) => this.matchesNameOrLastName(player,normalizedSearch));
+    let matchingPlayers = topPlayersMock.filter((player) => this.matchesNameOrLastName(player, normalizedSearch));
     return of(matchingPlayers);
   }
 
-  private matchesNameOrLastName(player : Player, search : string) : boolean {
-    if(!search) return false;
+  private matchesNameOrLastName(player: Player, search: string): boolean {
+    if (!search) return false;
     return player.name.toLowerCase().includes(search) || player.lastName.toLocaleLowerCase().includes(search)
+  }
+
+  getPlayerById(id : number) : Observable<Player> {
+    const matchingPlayer = topPlayersMock.find((player : Player) => player.id === id);
+    if (!matchingPlayer) {
+      throw new Error(`Player with id ${id} not found`);
+    }
+    return of(matchingPlayer);
   }
 }
