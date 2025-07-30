9f94cc04962885c6d7f3b7f663ff099b3f646776
benadjal <adame.benadjal@gmail.com>
2025-07-30 10:34:30 +0200

    ADD : Player detail new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PlayerDetailPageComponent } from './pages/player-detail-page/player-detail-page.component';
 
 export const routes: Routes = [
-{ path : '', component : HomePageComponent}
+{ path : '', component : HomePageComponent},
+{ path : 'player-detail', component : PlayerDetailPageComponent}
 ];
---
+++ src/app/pages/player-detail-page/player-detail-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-detail-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './player-detail-page.component.html',
+  styleUrl: './player-detail-page.component.scss'
+})
+export class PlayerDetailPageComponent {
+
+}
