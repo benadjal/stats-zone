3490d3824cb18629d0b1f7a4ffc81545f712c668
benadjal <adame.benadjal@gmail.com>
2025-08-01 19:33:37 +0200

    UPDATE : Go to player detail page from player card when clicking on check profil button

--- src/app/components/player-card/player-card.component.ts
+++ src/app/components/player-card/player-card.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { Player } from '../../models/player.model';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-player-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './player-card.component.html',
   styleUrl: './player-card.component.scss'
 })
 export class PlayerCardComponent {
 
   @Input() player! : Player;
+
 
 }
