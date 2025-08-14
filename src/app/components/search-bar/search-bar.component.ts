3bf309e6c7782bb49dd12107050e7be27d7ad9c1
benadjal <adame.benadjal@gmail.com>
2025-08-14 16:19:51 +0200

    REFACTO : code review refacto

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -21,40 +21,41 @@ import { Router } from '@angular/router';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
 
+export interface PlayerSelection {
+  player: Player;
+  mode: SearchBarMode
+}
+
+export type SearchBarMode = 'search' | 'comparePlayerOne' | 'comparePlayerTwo'
+
 @Component({
   selector: 'app-search-bar',
-  standalone: true,
   imports: [ReactiveFormsModule, AsyncPipe,InputTextModule,FloatLabelModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss',
 })
 export class SearchBarComponent {
-
-  @Input() mode: 'search' | 'comparePlayerOne' | 'comparePlayerTwo' = 'search';
-
-  @Output() playerSelected = new EventEmitter();
-
   playerService = inject(PlayersService);
 
   router = inject(Router);
 
-  searchForm: FormGroup = new FormGroup({
-    search: new FormControl('', Validators.required),
+  @Input() mode: 'search' | 'comparePlayerOne' | 'comparePlayerTwo' = 'search';
+
+  @Output() playerSelected = new EventEmitter<PlayerSelection>();
+
+  searchForm = new FormGroup({
+    search: new FormControl('', {nonNullable: true, validators: Validators.required}),
   });
 
-  filtredPlayers$: Observable<Player[]> = this.getPlayerList();
-
-  getPlayerList(): Observable<Player[]> {
-    return this.searchForm.get('search')!.valueChanges.pipe(
-      debounceTime(300),
-      distinctUntilChanged(),
-      switchMap((search) => this.playerService.getFilteredPlayers(search)),
-    );
-  }
+  filtredPlayers$: Observable<Player[]> = this.searchForm.controls.search.valueChanges.pipe(
+    debounceTime(300),
+    distinctUntilChanged(),
+    switchMap((search) => this.playerService.getFilteredPlayers(search)),
+  );
 
   selectPlayer(player: Player): void {
     if (this.mode === 'search') {
-      this.searchForm.controls['search'].setValue('');
+      this.searchForm.controls.search.reset();
       this.router.navigate(['player-detail', player.id]);
     } else {
       this.playerSelected.emit({ player: player, mode: this.mode });
--- src/app/components/stats-comparator/stats-comparator.component.ts
+++ src/app/components/stats-comparator/stats-comparator.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { SearchBarComponent } from '../search-bar/search-bar.component';
+import { PlayerSelection, SearchBarComponent } from '../search-bar/search-bar.component';
 import { Player } from '../../models/player.model';
 import { CardModule } from 'primeng/card';
 import { DividerModule } from 'primeng/divider';
@@ -7,31 +7,25 @@ import { ButtonModule } from "primeng/button";
 
 @Component({
   selector: 'app-compare-stats',
-  standalone: true,
   imports: [SearchBarComponent, CardModule, DividerModule, ButtonModule],
   templateUrl: './stats-comparator.component.html',
   styleUrl: './stats-comparator.component.scss',
 })
 export class StatsComparatorComponent {
-  isPlayerOneSelected: boolean = false;
-  isPlayerTwoSelected: boolean = false;
+  playerOne?: Player;
+  playerTwo?: Player;
 
-  playerOne!: Player;
-  playerTwo!: Player;
-
-  selectPlayer($event: any) {
-    if ($event.mode === 'comparePlayerOne') {
-      this.isPlayerOneSelected = true;
-      this.playerOne = $event.player;
-    } else if ($event.mode === 'comparePlayerTwo') {
-      this.isPlayerTwoSelected = true;
-      this.playerTwo = $event.player;
+  selectPlayer(playerSelection: PlayerSelection) {
+    if (playerSelection.mode === 'comparePlayerOne') {
+      this.playerOne = playerSelection.player;
+    } else if (playerSelection.mode === 'comparePlayerTwo') {
+      this.playerTwo = playerSelection.player;
     }
   }
 
   unSelectPlayer(firstOrSecondPlayer: 'playerOne' | 'playerTwo') {
     firstOrSecondPlayer === 'playerOne'
-      ? (this.isPlayerOneSelected = false)
-      : (this.isPlayerTwoSelected = false);
+      ? (this.playerOne = undefined)
+      : (this.playerTwo = undefined);
   }
 }
