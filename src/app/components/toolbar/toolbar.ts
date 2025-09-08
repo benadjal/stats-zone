2d8c91e6d8e25fe6aaf957cdc70d303455d8f425
benadjal <adame.benadjal@gmail.com>
2025-09-08 11:26:13 +0200

    UPDATE : On mobile changing theme is in menulist and hide switch button

--- src/app/components/toolbar/toolbar.ts
+++ src/app/components/toolbar/toolbar.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { Menubar } from 'primeng/menubar';
 import { ToggleSwitchModule } from 'primeng/toggleswitch';
@@ -9,7 +10,7 @@ import { ToggleSwitchModule } from 'primeng/toggleswitch';
 export interface MenuItems {
   label: string;
   icon?: string;
-  routerLink: string,
+  routerLink?: string,
   items?: MenuItems[]
 }
 
@@ -23,26 +24,40 @@ export interface MenuItems {
 
 export class Toolbar {
 
-
-  items: MenuItems[] = [
-    {
-      label: 'Top buteurs',
-      routerLink: "top-scorers"
-    }, {
-      label: 'Top passeurs',
-      routerLink: "top-assists"
-    },
-      {
-      label: 'Les plus sanctionnés',
-      routerLink: "most-carded"
-    }
-  ];
-
-
   checked: boolean = false;
+
+  isMobile = window.innerWidth <= 960;
+
+  items: MenuItem[] = [];
+
+  @HostListener('window:resize')
+  onResize() {
+    const isMobile = window.innerWidth <= 960;
+    const changerTheme = this.items.find(i => i.label === 'Changer de thème');
+    if (changerTheme) changerTheme.visible = isMobile;
+  }
+
+  ngOnInit() {
+    this.items = [
+      { label: 'Top buteurs', routerLink: "top-scorers" },
+      { label: 'Top passeurs', routerLink: "top-assists" },
+      { label: 'Les plus sanctionnés', routerLink: "most-carded" },
+      {
+        label: 'Changer de thème',
+        command: () => this.toggleDarkMode(),
+        visible: window.innerWidth <= 960   // init
+      }
+    ];
+  }
+
+
 
   toggleDarkMode() {
     const element = document.querySelector('html');
     element!.classList.toggle('app-dark');
+  }
+
+  displaly() {
+    console.log("ok")
   }
 }
